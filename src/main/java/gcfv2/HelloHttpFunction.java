37a2e157422e2a326390980bcedbdc03d4c1ce08
prajyotkcsu <154280801+prajyotkcsu@users.noreply.github.com>
2023-12-25 11:44:45 -0600

    Update HelloHttpFunction.java

--- src/main/java/gcfv2/HelloHttpFunction.java
+++ src/main/java/gcfv2/HelloHttpFunction.java
@@ -5,13 +5,11 @@ import java.io.BufferedWriter;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class HelloHttpFunction implements HttpFunction {
   public void service(final HttpRequest request, final HttpResponse response) throws Exception {
     final BufferedWriter writer = response.getWriter();
-    log.info("******Webhook triggered*****");
+    System.out.println("******Webhook triggered*****");
     writer.write("Hello world!");
   }
 }
