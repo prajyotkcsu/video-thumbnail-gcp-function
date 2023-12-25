0b1efc0f52c23ad183ab8cba02a2e9140eb09fa6
Prajyot Karnik <prajyotkcsu@gmail.com>
2023-12-25 12:18:12 -0600

    first commit

--- src/main/java/gcfv2/HelloHttpFunction.java
+++ src/main/java/gcfv2/HelloHttpFunction.java
@@ -5,13 +5,17 @@ import java.io.BufferedWriter;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class HelloHttpFunction implements HttpFunction {
   public void service(final HttpRequest request, final HttpResponse response) throws Exception {
     final BufferedWriter writer = response.getWriter();
-    log.info("******Webhook triggered*****");
+
+    System.out.println("******Webhook triggered*****");
+    System.out.println("******Webhook triggered-v2*****");
     writer.write("Hello world!");
+    String playbackUrl = "https://livepeer.studio/api/playback/09ee1iihioqf4zon";
+    AssetUploadController assetUploadController=new AssetUploadController();
+    String output=assetUploadController.fetchDataFromUrl(playbackUrl);
+    System.out.println("******Webhook triggered***** "+output);
   }
 }
