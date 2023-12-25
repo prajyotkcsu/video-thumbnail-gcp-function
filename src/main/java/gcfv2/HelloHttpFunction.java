38c8b9c8e264b11c6edc168c06a560e8e01ed755
Prajyot Karnik <prajyotkcsu@gmail.com>
2023-12-25 10:11:02 -0600

    first commit

---
+++ src/main/java/gcfv2/HelloHttpFunction.java
@@ -0,0 +1,17 @@
+package gcfv2;
+
+import java.io.BufferedWriter;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HelloHttpFunction implements HttpFunction {
+  public void service(final HttpRequest request, final HttpResponse response) throws Exception {
+    final BufferedWriter writer = response.getWriter();
+    log.info("******Webhook triggered*****");
+    writer.write("Hello world!");
+  }
+}
