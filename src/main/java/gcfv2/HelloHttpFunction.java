565054e0f469bb59cf67a9fb8792f7d6f73c3b1a
Prajyot Karnik <prajyotkcsu@gmail.com>
2023-12-26 10:52:10 -0600

    second commit

--- src/main/java/gcfv2/HelloHttpFunction.java
+++ src/main/java/gcfv2/HelloHttpFunction.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.*;
 import com.mongodb.client.model.Projections;
 import gcfv2.model.AssetDetails;
@@ -14,7 +16,11 @@ import gcfv2.model.PlaybackDetails;
 import gcfv2.model.UploadDetails;
 import gcfv2.video.VideoMetadata;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 
@@ -26,75 +32,66 @@ public class HelloHttpFunction implements HttpFunction {
 
     System.out.println("******Webhook triggered*****");
     System.out.println("******Webhook triggered-v2*****");
-    String connectionString = "mongodb+srv://livepeer-webhook:yYk4b7EobdvMD3ikLQKFKbAm5XXMfdYmNj2VYMDF2RzCDifsJJEPFFFuJTEQjcZ4TPkotj9wEGjuG8jPpPYLhoGrNDrGfDL6XX@orb-user-db.bxckw.mongodb.net/?retryWrites=true&w=majority";
-    MongoClient mongoClient = MongoClients.create(connectionString);
-    MongoDatabase database = mongoClient.getDatabase("orbdb");
-    MongoCollection<Document> collection = database.getCollection("upload-video");
-    writer.write("Hello world!");
+    String connectionStrings = Optional.ofNullable(System.getenv("CONNECTION_STRING"))
+            .orElseThrow(() -> new IllegalStateException("CONNECTION_STRING environment variable not set."));
+
+    log.info("Connection s" + connectionStrings);
+    CodecRegistry codecRegistry = CodecRegistries.fromRegistries(
+            MongoClientSettings.getDefaultCodecRegistry(),
+            CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())
+    );
+
+    MongoClientSettings settings = MongoClientSettings.builder()
+            .codecRegistry(codecRegistry)
+            .applyConnectionString(new ConnectionString(connectionStrings))
+            .build();
+
+    MongoClient mongoClient = MongoClients.create(settings);
+
+    String database_name = Optional.ofNullable(System.getenv("DATABASE"))
+            .orElseThrow(() -> new IllegalStateException("DATABASE environment variable not set."));
+
+    String collection_name = Optional.ofNullable(System.getenv("COLLECTION"))
+            .orElseThrow(() -> new IllegalStateException("COLLECTION environment variable not set."));
+    MongoDatabase database = mongoClient.getDatabase(database_name);
+    MongoCollection<Document> collection = database.getCollection(collection_name);
 
     ObjectMapper objectMapper = new ObjectMapper();
     AssetDetails assetDetails = null;
     PlaybackDetails playbackDetails = null;
-    UploadDetails asset=null;
+    UploadDetails asset = null;
     String requestBody = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
-    log.info("requestBody: "+requestBody);
+    log.info("New message from Livepeer " + requestBody);
     String eventType = objectMapper.readTree(requestBody).get("event").asText();
     if (eventType.equals("asset.ready")) {
-    String assetId = objectMapper.readTree(requestBody).get("payload").get("id").asText();
-    log.info("assetId: "+assetId);
-    Document query = new Document("assetId","a1188fc2-4bbf-45de-8050-0e5ef405c7e1");
-    FindIterable<Document> docs = collection.find(query);
-      log.info("Doc"+docs);
+      String assetId = objectMapper.readTree(requestBody).get("payload").get("id").asText();
+      log.info("assetId: " + assetId);
+      Document query = new Document("assetId", "a1188fc2-4bbf-45de-8050-0e5ef405c7e1");
+      FindIterable<Document> docs = collection.find(query);
+      log.info("Doc" + docs);
       String playbackId = null;
-      for(Document doc:docs){
-        log.info("infom"+doc.toJson());
-        log.info("+"+doc.get("playbackId"));
-         playbackId = doc.getString("playbackId");
-         log.info("pay: "+playbackId);
+      for (Document doc : docs) {
+        log.info("Doc representation" + doc.toJson());
+        playbackId = doc.getString("playbackId");
         log.info("******playbackId******" + playbackId);
         assert asset != null;
-        String playbackUrl = "https://livepeer.studio/api/playback/"+playbackId;
-        AssetUploadController assetUploadController=new AssetUploadController();
-        String output=assetUploadController.fetchDataFromUrl(playbackUrl);
+        String playbackUrl = "https://livepeer.studio/api/playback/" + playbackId;
+        AssetUploadController assetUploadController = new AssetUploadController();
+        String output = assetUploadController.fetchDataFromUrl(playbackUrl);
+        log.info("output: " + output);
         VideoMetadata videoMetadata = objectMapper.readValue(output, VideoMetadata.class);
         playbackDetails = videoMetadata.extractVideoList(videoMetadata);
-        doc.put("source",playbackDetails.getSource());
-        doc.put("playbackURL",playbackDetails.getPlaybackURL());
-        doc.put("thumbnails",playbackDetails.getThumbnails());
-        doc.put("livepeer:",false);
-        doc.put("timeModified:",System.currentTimeMillis());
-      /*Document updatedDocument = Document.parse(objectMapper.writeValueAsString(asset));
-      collection.replaceOne(query, updatedDocument);*/
-        log.info("Asset ready: "+ asset);
-        log.info("Asset uploaded!!!");
+        doc.put("source", playbackDetails.getSource());
+        doc.put("playbackURL", playbackDetails.getPlaybackURL());
+        doc.put("thumbnails", playbackDetails.getThumbnails());
+        doc.put("livepeer:", true);
+        doc.put("timeModified:", System.currentTimeMillis());
+
         collection.replaceOne(query, doc);
-        log.info("DOc"+doc);
+        log.info("Asset uploaded and thumbnails are ready!!!");
+        mongoClient.close();
+
       }
-      /*for (Document doc : result) {
-
-      String record = doc.toJson();
-      try {
-        log.info("record: "+record);
-        asset =objectMapper.readValue(record, UploadDetails.class);
-      } catch (JsonProcessingException e) {
-        throw new RuntimeException(e);
-      }
-    }*/
-
-
-
-
-      mongoClient.close();
-    /*asset.setSource(playbackDetails.getSource());
-    asset.setPlaybackURL(playbackDetails.getPlaybackURL());
-    asset.setThumbnails(playbackDetails.getThumbnails());
-    asset.setTranscodingCompleted(true);
-    asset.setTimeModified(System.currentTimeMillis());
-    Document updatedDocument = Document.parse(objectMapper.writeValueAsString(asset));
-    collection.replaceOne(query, updatedDocument);
-    mongoClient.close();
-    log.info("Asset ready: "+ asset);
-    log.info("Asset uploaded!!!");*/
+    }
   }
-}
 }
